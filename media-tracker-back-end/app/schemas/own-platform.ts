6818f7eedf798a715bfe561b15603cbee4100ba4
Simone Graziussi <simone.graziussi@gmail.com>
2019-05-01 12:21:43 +0200

    Add schema for own platforms

---
+++ media-tracker-back-end/app/schemas/own-platform.ts
@@ -0,0 +1,20 @@
+import { CATEGORY_COLLECTION_NAME } from 'app/schemas/category';
+import { USER_COLLECTION_NAME } from 'app/schemas/user';
+import { Schema } from 'mongoose';
+
+/**
+ * Database schema for platforms where some user owns some media item
+ */
+export const OwnPlatformSchema: Schema = new Schema({
+	name: { type: String, required: true },
+	color: { type: String, required: true },
+	owner: { type: Schema.Types.ObjectId, ref: USER_COLLECTION_NAME, required: true },
+	category: { type: Schema.Types.ObjectId, ref: CATEGORY_COLLECTION_NAME, required: true }
+}, {
+	timestamps: true
+});
+
+/**
+ * OwnPlatforms collection name
+ */
+export const OWN_PLATFORM_COLLECTION_NAME = 'OwnPlatform';
