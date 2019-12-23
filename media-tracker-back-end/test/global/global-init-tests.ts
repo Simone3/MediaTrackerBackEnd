099465c7061ec8b5532fde351da2f65b5c0be9c8
Simone <simone.graziussi@gmail.com>
2019-12-23 12:25:18 +0100

    Mock Firebase with sinon for integration tests

--- media-tracker-back-end/test/global/global-init-tests.ts
+++ media-tracker-back-end/test/global/global-init-tests.ts
@@ -1,5 +1,9 @@
 import 'reflect-metadata';
+import { setupTestAuth } from 'helpers/auth-handler-helper';
 
 // Overwrite the env property, for configurations
 // eslint-disable-next-line no-process-env
 process.env.MEDIA_TRACKER_BE_ENV = 'test';
+
+// Setup global mocks and spies
+setupTestAuth();
