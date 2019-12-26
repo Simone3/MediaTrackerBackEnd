a3711c6b2dde0c3dc60171a2738628145c591d8d
Simone <simone.graziussi@gmail.com>
2019-12-26 09:43:47 +0100

    Add global test handler for unhandled promise rejections to avoid silent failures

--- media-tracker-back-end/test/global/global-init-tests.ts
+++ media-tracker-back-end/test/global/global-init-tests.ts
@@ -5,5 +5,10 @@ import { setupTestAuth } from 'helpers/auth-handler-helper';
 // eslint-disable-next-line no-process-env
 process.env.MEDIA_TRACKER_BE_ENV = 'test';
 
+// Catch all "unhandledRejection" warnings
+process.on('unhandledRejection', (error) => {
+	throw error;
+});
+
 // Setup global mocks and spies
 setupTestAuth();
