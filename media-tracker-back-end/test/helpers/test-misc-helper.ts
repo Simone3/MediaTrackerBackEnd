82a312569ca0cd47241f59f0957b261fd39d5339
Simone <simone.graziussi@gmail.com>
2019-12-26 09:43:13 +0100

    Add tests for the import of the old Media Tracker app export

--- media-tracker-back-end/test/helpers/test-misc-helper.ts
+++ media-tracker-back-end/test/helpers/test-misc-helper.ts
@@ -1,3 +1,6 @@
+import chai from 'chai';
+
+const expect = chai.expect;
 
 /**
  * Helper to generate a random name
@@ -36,4 +39,31 @@ export const extractAsString = function<V extends object>(array: V[], field: key
 
 		return String(value[field]);
 	});
+};
+
+/**
+ * Helper to compare only the given fields of the "value" and "expected" objects
+ * @param value the test result object
+ * @param expected the expected result object
+ * @param fields the fields to compare in the two objects
+ * @param message the error
+ */
+export const comparePickedFields = function<V extends E, E extends object>(value: V, expected: E, fields: (keyof E)[], message: string): void {
+
+	for(const field of fields) {
+
+		expect(value[field], `${message} [comparing field '${field}']`).to.be.eql(expected[field]);
+	}
+};
+
+/**
+ * Helper to compare the "value" and "expected" objects considering only the "expected" fields.
+ * E.g. with value = {a:1, b:2, c:3} and expected = {a:1, c:3} the comparison succeeds because only "a" and "c" fields are compared between the two.
+ * @param value the test result object
+ * @param expected the expected result object
+ * @param message the error
+ */
+export const compareExpectedFields = function<V extends E, E extends object>(value: V, expected: E, message: string): void {
+
+	comparePickedFields(value, expected, Object.keys(expected) as (keyof E)[], message);
 };
