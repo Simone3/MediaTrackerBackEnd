a3f493a477419b1276eef39b13b0ed616bfd0a81
Simone <simone.graziussi@gmail.com>
2020-01-01 11:19:09 +0100

    Remove useless eslint-disable-next-line comments

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-// eslint-disable-next-line import/no-unassigned-import
 import 'reflect-metadata';
-// eslint-disable-next-line import/no-internal-modules
 import { init } from './app/app';
 
 init();
